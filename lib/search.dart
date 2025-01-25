8ded8928df84da8b07928ebdcb4b628c90bfd745
Rishav Kumar Das <rishav.k.das@gmail.com>
2025-01-25 23:08:22 +0530

    vital-changes
    removed video_player and url_launcher import since they were creating issues with jdk and were obsolete

--- lib/search.dart
+++ lib/search.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:intl/intl.dart';
-import 'package:url_launcher/url_launcher.dart';
+
 
 class Search extends StatefulWidget {
   const Search({super.key});
